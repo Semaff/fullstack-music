781d07f4a16a177e2c121964ec38d2f423ffa8f5
Semyon <semarc-zhu@yandex.ru>
2022-10-30 11:37:26 +0300

    Created SignUp dto

---
+++ server/src/models/user/dto/signup-user.dto.ts
@@ -0,0 +1,25 @@
+import { IsString, IsEmail, MinLength, MaxLength, Matches } from "class-validator";
+
+export class SignUpUserDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  firstName: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      "Password is too weak! (Must contain at least 1: upper-cased letter, number, special character)"
+  })
+  password: string;
+}
