09ba44574492c578403454cc1dce21b46ce966f0
Semyon <semarc-zhu@yandex.ru>
2022-10-30 11:40:43 +0300

    Created ServeFiles decorator

---
+++ server/src/common/decorators/serve-file.decorator.ts
@@ -0,0 +1,21 @@
+import { applyDecorators, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { generateRandomName } from "src/common/utils/generateRandomName";
+import * as path from "path";
+
+export function ServeFile(fileName: string) {
+  return applyDecorators(
+    UseInterceptors(
+      FileInterceptor(fileName, {
+        storage: diskStorage({
+          destination: path.resolve(__dirname, "..", "..", "..", "static"),
+          filename: (req, file, cb) => {
+            const randomName = generateRandomName();
+            return cb(null, `${randomName}${path.extname(file.originalname)}`);
+          }
+        })
+      })
+    )
+  );
+}
