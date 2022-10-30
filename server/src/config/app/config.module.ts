9d4764e16dc723f026617637efd9a9aea0aca8b8
Semyon <semarc-zhu@yandex.ru>
2022-10-30 11:36:13 +0300

    Created config module

---
+++ server/src/config/app/config.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { ThrottlerModule } from "@nestjs/throttler";
+import { MulterModule } from "@nestjs/platform-express";
+import * as path from "path";
+
+const ENV = process.env.NODE_ENV;
+
+@Module({
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10 // amount of requests in `ttl seconds`
+    }),
+
+    ConfigModule.forRoot({
+      envFilePath: !ENV ? ".env" : `.env.${ENV}`,
+      cache: true
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, "..", "..", "..", "static")
+    }),
+
+    MulterModule.register({
+      dest: path.resolve(__dirname, "..", "..", "..", "static")
+    })
+  ]
+})
+export class ConfigurationProvider {}
