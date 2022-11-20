d26b4be9ea681605182b82fadf6623b26e89bdce
Semyon <semarc-zhu@yandex.ru>
2022-11-20 17:49:19 +0300

    Create validations

---
+++ client/src/utils/validations/validatePassword.ts
@@ -0,0 +1,6 @@
+import { passwordRegexp } from "utils/regexps/passwordRegexp";
+
+export const validatePassword = (password: string) => {
+  const isCorrectPassword = passwordRegexp.test(password);
+  return isCorrectPassword && password.length > 3 && password.length < 20;
+};
