6cbd38737ab9a69128b0e7122a4fe9b92bfe42d6
Semen <salatishkobadik@gmail.com>
2022-12-02 14:02:41 +0300

    Fix paths

--- server/src/config/database/typeorm.module.ts
+++ server/src/config/database/typeorm.module.ts
@@ -1,12 +1,12 @@
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "src/models/user/entities/user.entity";
-import { Group } from "src/models/group/entities/group.entity";
-import { Profile } from "src/models/profile/entities/profile.entity";
-import { Album } from "src/models/album/entities/album.entity";
-import { Track } from "src/models/track/entities/track.entity";
-import { Playlist } from "src/models/playlist/entities/playlist.entity";
+import { User } from "src/modules/auth/entities/user.entity";
+import { Group } from "src/modules/group/entities/group.entity";
+import { Profile } from "src/modules/profile/entities/profile.entity";
+import { Album } from "src/modules/album/entities/album.entity";
+import { Track } from "src/modules/track/entities/track.entity";
+import { Playlist } from "src/modules/playlist/entities/playlist.entity";
 
 @Module({
   imports: [
--- server/src/config/modules/internal.module.ts
+++ server/src/config/modules/internal.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
-import { AlbumModule } from "src/models/album/album.module";
-import { GroupModule } from "src/models/group/group.module";
-import { PlaylistModule } from "src/models/playlist/playlist.module";
-import { ProfileModule } from "src/models/profile/profile.module";
-import { TrackModule } from "src/models/track/track.module";
-import { UserModule } from "src/models/user/user.module";
+import { AlbumModule } from "src/modules/album/album.module";
+import { GroupModule } from "src/modules/group/group.module";
+import { PlaylistModule } from "src/modules/playlist/playlist.module";
+import { ProfileModule } from "src/modules/profile/profile.module";
+import { TrackModule } from "src/modules/track/track.module";
+import { AuthModule } from "src/modules/auth/auth.module";
 
 @Module({
-  imports: [UserModule, ProfileModule, GroupModule, TrackModule, AlbumModule, PlaylistModule]
+  imports: [AuthModule, ProfileModule, GroupModule, TrackModule, AlbumModule, PlaylistModule]
 })
 export class InternalModulesProvider {}
