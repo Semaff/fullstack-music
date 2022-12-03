b3a1e305274f8dffea633bfdf4914a99f124b37a
Semen <salatishkobadik@gmail.com>
2022-12-03 16:31:52 +0300

    Create custom _app

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import "@styles/globals.scss";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
