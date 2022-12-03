f15676d0abda9b5276586be575354c003b0cb600
Semen <salatishkobadik@gmail.com>
2022-12-03 21:18:17 +0300

    Remove useless imports

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,11 +1,7 @@
-import { Module, forwardRef, Global } from "@nestjs/common";
+import { Module, Global } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { JwtModule } from "@nestjs/jwt";
-import { AlbumModule } from "../album/album.module";
-import { PlaylistModule } from "../playlist/playlist.module";
-import { TrackModule } from "../track/track.module";
-import { ProfileModule } from "../profile/profile.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
