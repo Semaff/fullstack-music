3a6baced2adeaa865d2d18275ddbcbea562f1646
Semen <salatishkobadik@gmail.com>
2022-12-11 11:03:32 +0300

    Allow cross-origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   /* Protection */
-  app.use(helmet());
+  app.use(helmet({ crossOriginResourcePolicy: false }));
 
   app.use(cookieParser());
   app.enableCors({ credentials: true, origin: "http://localhost:3000" });
