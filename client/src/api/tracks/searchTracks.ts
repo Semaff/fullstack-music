450fcfb6dd9ccebe926f1d504a8078ca7615758d
Semen <salatishkobadik@gmail.com>
2022-12-11 11:01:27 +0300

    Create the remaining API calls for tracks

---
+++ client/src/api/tracks/searchTracks.ts
@@ -0,0 +1,15 @@
+import { request } from "@api/requests";
+import { ITrack } from "@typings/tracks/ITrack";
+import { AxiosRequestHeaders } from "axios";
+
+export const searchTracks = async (token: string | undefined, search?: string) => {
+  const headers: Partial<AxiosRequestHeaders> = {};
+  if (token && token.length > 0) {
+    headers.cookie = `token=${token}`;
+  }
+
+  const response = await request.get<ITrack[]>(`/track?search=${search}`, {
+    headers
+  });
+  return response.data;
+};
--- client/src/types/tracks/ITrack.ts
+++ client/src/types/tracks/ITrack.ts
@@ -2,4 +2,11 @@ export interface ITrack {
   id: number;
   name: string;
   file: string;
+  user: {
+    firstName: string;
+    lastName: string;
+    profile: {
+      nickname: string;
+    };
+  };
 }
