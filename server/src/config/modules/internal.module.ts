8d2bd5ce4e415bf173c7f6b14ad318640842bb59
Semen <salatishkobadik@gmail.com>
2022-12-14 15:04:27 +0300

    Import playlist module

--- server/src/config/modules/internal.module.ts
+++ server/src/config/modules/internal.module.ts
@@ -3,8 +3,9 @@ import { ProfileModule } from "src/modules/profile/profile.module";
 import { TrackModule } from "src/modules/track/track.module";
 import { AuthModule } from "src/modules/auth/auth.module";
 import { AlbumModule } from "src/modules/album/album.module";
+import { PlaylistModule } from "src/modules/playlist/playlist.module";
 
 @Module({
-  imports: [AuthModule, ProfileModule, TrackModule, AlbumModule]
+  imports: [AuthModule, ProfileModule, TrackModule, AlbumModule, PlaylistModule]
 })
 export class InternalModulesProvider {}
