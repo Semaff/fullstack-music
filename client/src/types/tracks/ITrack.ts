c7173c6d7723a8f7b33c2de98283b07055e25a0c
Semen <salatishkobadik@gmail.com>
2022-12-30 21:17:09 +0300

    Fix track typing

--- client/src/types/tracks/ITrack.ts
+++ client/src/types/tracks/ITrack.ts
@@ -1,15 +1,10 @@
 import { IComment } from "@typings/comment/IComment";
+import { IUser } from "@typings/user/IUser";
 
 export interface ITrack {
   id: number;
   name: string;
   file: string;
-  user: {
-    firstName: string;
-    lastName: string;
-    profile: {
-      nickname: string;
-    };
-  };
+  user: IUser;
   comments: IComment[];
 }
